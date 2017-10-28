d303916c0d8a56f78eaa5239cb65a55239a6e9cb
spencerdodd <spencer.evan.dodd@gmail.com>
2017-10-28 17:40:42 -0400

    major overhaul on version checking...should work now

--- src/kernels.py
+++ src/kernels.py
@@ -2,6 +2,7 @@
 Class for holding kernel windows for vulnerabilities
 """
 from src.kernelpop import Kernel
+from constants import CONFIRMED_VULNERABLE, NOT_VULNERABLE
 
 class KernelWindow:
 	def __init__(self, distro, confirmation, lowest_major, lowest_minor, lowest_release, highest_major, highest_minor,
@@ -23,9 +24,34 @@ class KernelWindow:
 		"""
 		# check for self.distro in kernel.distro so that we can match generics to all kernels of a type:
 		# 	i.e. "debian" will be in "debian7" and "debian8" etc...
-		if self.distro in kernel.distro and self.lowest_major <= kernel.major_version <= self.highest_major and \
-			self.lowest_minor <= kernel.minor_version <= self.highest_minor and self.lowest_release <= kernel.release\
-			<= self.highest_release:
-			return self.confirmation
-		else:
+		# TODO: this logic is fucked
+		if not self.distro in kernel.distro:
 			return "no"
+		else:
+			if self.lowest_major <= kernel.major_version <= self.highest_major:
+				if self.highest_major == kernel.major_version:
+					if self.highest_minor == kernel.minor_version:
+						if self.highest_release >= kernel.release:
+							return self.confirmation
+						else:
+							return "no"
+					elif self.highest_minor > kernel.minor_version:
+						return self.confirmation
+					else:
+						return "no"
+				elif self.highest_major > kernel.major_version:
+					return self.confirmation
+				elif self.lowest_major == kernel.major_version:
+					if self.lowest_minor == kernel.minor_version:
+						if self.lowest_release <= kernel.release:
+							return self.confirmation
+						else:
+							return "no"
+					elif self.lowest_minor < kernel.minor_version:
+						return self.confirmation
+					else:
+						return "no"
+				elif self.lowest_major < kernel.major_version:
+					return self.confirmation
+			else:
+				return "no"
