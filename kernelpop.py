e3ed314ea68a7e7021f80136597030177b0d95a4
spencerdodd <spencer.evan.dodd@gmail.com>
2017-11-06 19:43:00 -0500

    added exploit mode flag

--- kernelpop.py
+++ kernelpop.py
@@ -8,6 +8,8 @@ def main():
 	# brute force all discovered exploits
 	elif sys.argv[1] == "-b":
 		kernelpop(mode="brute-enumerate")
+	elif sys.argv[1] == "-e":
+		kernelpop(mode="brute-exploit")
 	elif sys.argv[1] == "-i":
 		uname = input("Please enter uname: ")
 		kernelpop(mode="input", uname=uname)
