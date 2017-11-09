2f654d055de5fde802d4b39c9c9430a4e6876247
spencerdodd <spencer.evan.dodd@gmail.com>
2017-11-08 19:03:39 -0500

    fixed issue when parsing from uname and grabbing underlying system info. still need to rectify issue with mac version labeling for exploits (OS vs kernel). Need different uname -a support for mac as we need to get the version number.

--- src/kernelpop.py
+++ src/kernelpop.py
@@ -136,7 +136,9 @@ class Kernel:
 				k_type = "mac"
 				k_distro = self.parse_distro(kernel_version)
 				k_name = kernel_version.split(" ")[0]
-				k_major, k_minor, k_release = self.get_mac_version()
+				k_major = int(kernel_version.split("-")[1].split(".")[0])
+				k_minor = int(kernel_version.split("-")[1].split(".")[1])
+				k_release = int(kernel_version.split("-")[1].split(".")[2])
 				k_architecture = kernel_version.split(" ")[-1]
 				# replace any bad architecture parses
 				for architecture in ["x86", "i686", "amd64", "x86_64"]:
@@ -148,9 +150,7 @@ class Kernel:
 				k_type = "mac"
 				k_distro = self.parse_distro(kernel_version)
 				k_name = kernel_version.split("-")[0]
-				k_major = int(kernel_version.split("-")[1].split(".")[0])
-				k_minor = int(kernel_version.split("-")[1].split(".")[1])
-				k_release = int(kernel_version.split("-")[1].split(".")[2])
+				k_major, k_minor, k_release = self.get_mac_version()
 				k_architecture = kernel_version.split("-")[2]
 				# replace any bad architecture parses
 				for architecture in ["x86", "i686", "amd64", "x86_64"]:
