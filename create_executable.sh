42e4936f39e4a01e49937cb4b2f0bbc5db90519f
spencerdodd <spencer.evan.dodd@gmail.com>
2018-04-11 02:33:53 -0400

    fixed executable build script and modified manual instructions

--- create_executable.sh
+++ create_executable.sh
@@ -17,27 +17,14 @@ echo "[*] installing pyinstaller"
 pip install pyinstaller || echo "[!] could not install pyinstaller"
 
 
-single_file="$(pwd)/singlefile.py"
-echo "[*] compiling project into single file at $single_file"
-dirs=('exploits' 'exploits/linux' 'exploits/mac' 'exploits/windows' 'src' 'constants.py' 'kernelpop.py')
-echo "" > $single_file
-for dir in ${dirs[@]}; do
-    current_find_dir="$dir"
-    echo "[*] searching in $dir"
-    for file in $(find $(pwd)/$dir -name '*.py'); do
-        cat $file >> $single_file
-        echo "" >> $single_file
-    done
-done
-
 echo "[*] building the executable"
-pyinstaller singlefile.py --onefile
+pyinstaller kernelpop.py --onefile
 
-echo "[*] copying to project directory"
-cp dist/singlefile kernelpop
+echo "[*] copying executable to project root"
+cp dist/kernelpop .
 
 echo "[*] cleaning up build"
-rm -r $(pwd)/build $(pwd)/dist $(pwd)/singlefile.spec $(pwd)/singlefile.py
+rm -r $(pwd)/build $(pwd)/dist $(pwd)/kernelpop.spec
 
 executable_location="$(pwd)/kernelpop"
 echo "[+] executable at $executable_location"
